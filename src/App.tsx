43c9a2c4180fca9e64b764b26472e0536f9d9bf1
Austin Wang <austin.sa.wang@gmail.com>
2023-04-24 10:58:14 +0800

    implement working sandbox with react and webgl

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+  });
+
+  return <div className="fullscreen" ><Unity className="w-full h-full" unityProvider={unityProvider} /></div>;
+}
+
+export default App
